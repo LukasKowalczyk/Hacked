6a25106cb60faebe430d74e87973c995c35c3a36
Lukas Kowalczyk <pd06286@prin.loc>
2017-12-21 15:29:37 +0100

    Views angepasst

--- src/main/java/com/hacked/view/LobbyView.java
+++ src/main/java/com/hacked/view/LobbyView.java
@@ -2,9 +2,10 @@ package com.hacked.view;
 
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.github.appreciated.material.MaterialTheme;
 import com.hacked.controller.Broadcaster;
-import com.hacked.controller.GameService;
 import com.hacked.controller.Broadcaster.BroadcastListener;
+import com.hacked.controller.GameService;
 import com.hacked.controller.HackedSessionService;
 import com.hacked.controller.PlayerService;
 import com.hacked.controller.UtilityService;
@@ -25,159 +26,160 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.renderers.HtmlRenderer;
 
 /**
- * @author  
+ * @author
  */
 @SpringView(name = LobbyView.VIEW_NAME)
 @Push
 public class LobbyView extends VerticalLayout implements View, BroadcastListener {
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	@Autowired
-	private GameService gameService;
-	
-	@Autowired
-	private UtilityService utilityService;
-	
-	@Autowired
-	private PlayerService playerService;
+    @Autowired
+    private GameService gameService;
 
-	public static final String VIEW_NAME = "lobby";
+    @Autowired
+    private UtilityService utilityService;
 
-	private Grid<Player> myGrid;
+    @Autowired
+    private PlayerService playerService;
 
-	private Button startGame;
+    public static final String VIEW_NAME = "lobby";
 
-	private Label playerReady;
+    private Grid<Player> myGrid;
 
-	private Button readyButton;
+    private Button startGame;
 
-	private Player player;
+    private Label playerReady;
 
-	@PostConstruct
-	void init() {
-		player = HackedSessionService.getPlayer();
-		String gameId = player.getGameId();
+    private Button readyButton;
 
-		playerReady = new Label("Warte auf Spieler");
-		if (!playerService.isMinPlayerCountOfGame(gameId)) {
-			playerReady.setValue("Zu wenig Spieler (Min. 3)");
-		}
+    private Player player;
 
-		myGrid = generatePlayerTable(gameId, player);
-		reloadListe(myGrid, gameId);
+    @PostConstruct
+    void init() {
+        this.setSizeUndefined();
+        player = HackedSessionService.getPlayer();
+        String gameId = player.getGameId();
 
-		startGame = new Button("Start Game");
-		startGame.setVisible(playerService.isPlayerMasterOfGame(player.getId(), gameId));
-		startGame.setEnabled(playerService.playerOfGameReady(gameId));
-		startGame.addClickListener(e -> {
-			reloadListe(myGrid, gameId);
-			startGame(player.getId(), gameId);
-		});
+        playerReady = new Label("Warte auf Spieler");
+        if (!playerService.isMinPlayerCountOfGame(gameId)) {
+            playerReady.setValue("Zu wenig Spieler (Min. 3)");
+        }
 
-		readyButton = new Button("Bereit");
-		readyButton.setEnabled(playerService.isMinPlayerCountOfGame(gameId));
-		readyButton.setDisableOnClick(true);
-		readyButton.addClickListener(e -> {
-			setPlayerReady(myGrid, player);
-		});
+        myGrid = generatePlayerTable(gameId, player);
+        reloadListe(myGrid, gameId);
 
-		addComponents(playerReady, myGrid, new HorizontalLayout(readyButton, startGame));
-		Broadcaster.register(gameId, this);
-	}
+        startGame = new Button("Start Game");
+        startGame.addStyleName(MaterialTheme.BUTTON_BORDERLESS);
+        startGame.setVisible(playerService.isPlayerMasterOfGame(player.getId(), gameId));
+        startGame.setEnabled(playerService.playerOfGameReady(gameId));
+        startGame.addClickListener(e -> {
+            reloadListe(myGrid, gameId);
+            startGame(player.getId(), gameId);
+        });
 
-	@Override
-	public void detach() {
-		Broadcaster.unregister(player.getGameId(), this);
-		super.detach();
-	}
+        readyButton = new Button("Bereit");
+        readyButton.addStyleName(MaterialTheme.BUTTON_BORDERLESS);
+        readyButton.setEnabled(playerService.isMinPlayerCountOfGame(gameId));
+        readyButton.setDisableOnClick(true);
+        readyButton.addClickListener(e -> {
+            setPlayerReady(myGrid, player);
+        });
 
-	private Grid<Player> generatePlayerTable(String gameId, Player player) {
-		Grid<Player> myGrid = new Grid<>();
-		myGrid.setHeight("300px");
-		myGrid.setWidth("250px");
-		myGrid.addColumn(p -> ladeReadyImage(p), new HtmlRenderer()).setCaption(" ").setMaximumWidth(50);
-		myGrid.addColumn(Player::getName).setCaption("Name");
-		myGrid.addColumn(p -> ladeMasterImage(p, gameId), new HtmlRenderer()).setCaption(" ").setMaximumWidth(50);
-		myGrid.select(player);
-		return myGrid;
-	}
+        addComponents(playerReady, myGrid, new HorizontalLayout(readyButton, startGame));
+        Broadcaster.register(gameId, this);
+    }
 
-	private String ladeMasterImage(Player p, String gameId) {
-		if (playerService.isPlayerMasterOfGame(p.getId(), gameId)) {
-			return VaadinIcons.KEY.getHtml();
-		}
-		return "";
-	}
+    @Override
+    public void detach() {
+        Broadcaster.unregister(player.getGameId(), this);
+        super.detach();
+    }
 
-	/**
-	 * @param allPlayerReady
-	 */
-	private void startGame(long playerId, String gameId) {
-		if (!playerService.playerOfGameReady(gameId)) {
-			return;
-		}
-		if (!gameService.isGameReadyRunning(gameId)) {
-			gameService.startGame(gameId);
-		}
-		generateRoleMeldung(playerId);
-		UI.getCurrent().getNavigator().navigateTo(RundeView.VIEW_NAME);
-		Broadcaster.broadcast(player.getGameId(), "Spiel Startet");
-	}
+    private Grid<Player> generatePlayerTable(String gameId, Player player) {
 
-	private void generateRoleMeldung(long playerId) {
-		Role role = playerService.getPlayer(playerId).getRole();
-		utilityService.generateMeldung("Deine Rolle", role.getIcon().getHtml() + " :: " + role.getTitel())
-				.show(Page.getCurrent());
-	}
+        Grid<Player> myGrid = new Grid<>();
+        myGrid.removeHeaderRow(0);
+        myGrid.setStyleName(MaterialTheme.TABLE_BORDERLESS);
+        myGrid.addColumn(p -> ladeReadyImage(p), new HtmlRenderer()).setMaximumWidth(50);
+        myGrid.addColumn(Player::getName);
+        myGrid.addColumn(p -> ladeMasterImage(p, gameId), new HtmlRenderer()).setMaximumWidth(50);
+        myGrid.select(player);
+        return myGrid;
+    }
 
-	private void setPlayerReady(Grid<Player> myGrid, Player player) {
-		playerService.setPlayerReady(player.getId());
-		Broadcaster.broadcast(player.getGameId(), "Player " + player.getName() + " ist Bereit");
-		myGrid.setItems(playerService.getPlayerOfGame(player.getGameId()));
-	}
+    private String ladeMasterImage(Player p, String gameId) {
+        if (playerService.isPlayerMasterOfGame(p.getId(), gameId)) {
+            return VaadinIcons.KEY.getHtml();
+        }
+        return "";
+    }
 
-	private void reloadListe(Grid<Player> myGrid, String gameId) {
-		myGrid.setItems(playerService.getPlayerOfGame(gameId));
-	}
+    /**
+     * @param allPlayerReady
+     */
+    private void startGame(long playerId, String gameId) {
+        if (!playerService.playerOfGameReady(gameId)) {
+            return;
+        }
+        if (!gameService.isGameReadyRunning(gameId)) {
+            gameService.startGame(gameId);
+        }
+        generateRoleMeldung(playerId);
+        UI.getCurrent().getNavigator().navigateTo(RundeView.VIEW_NAME);
+        Broadcaster.broadcast(player.getGameId(), "Spiel Startet");
+    }
 
-	private String ladeReadyImage(Player p) {
-		if (p.isReady()) {
-			return VaadinIcons.CHECK_CIRCLE_O.getHtml();
-		}
-		return VaadinIcons.CIRCLE_THIN.getHtml();
-	}
+    private void generateRoleMeldung(long playerId) {
+        Role role = playerService.getPlayer(playerId).getRole();
+        utilityService.generateMeldung("Deine Rolle", role.getIcon().getHtml() + " :: " + role.getTitel())
+            .show(Page.getCurrent());
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.
-	 * ViewChangeEvent)
-	 */
-	@Override
-	public void enter(ViewChangeEvent event) {
-		// Auto-generated method stub
-	}
+    private void setPlayerReady(Grid<Player> myGrid, Player player) {
+        playerService.setPlayerReady(player.getId());
+        Broadcaster.broadcast(player.getGameId(), "Player " + player.getName() + " ist Bereit");
+        myGrid.setItems(playerService.getPlayerOfGame(player.getGameId()));
+    }
 
-	@Override
-	public void receiveBroadcast(String message) {
-		getUI().access(() -> {
-			String gameId = player.getGameId();
-			readyButton.setEnabled(playerService.isMinPlayerCountOfGame(gameId));
-			reloadListe(myGrid, gameId);
-			if (!playerService.isMinPlayerCountOfGame(gameId)) {
-				playerReady.setValue("Zu wenig Spieler (Min. 3)");
-			} else {
-				playerReady.setValue("Warte auf Spieler");
-			}
-			if (playerService.playerOfGameReady(gameId)) {
-				startGame.setEnabled(true);
-				playerReady.setValue("Alle Spieler Bereit");
-			}
-			startGame(player.getId(), gameId);
-		});
-	}
+    private void reloadListe(Grid<Player> myGrid, String gameId) {
+        myGrid.setItems(playerService.getPlayerOfGame(gameId));
+    }
+
+    private String ladeReadyImage(Player p) {
+        if (p.isReady()) {
+            return VaadinIcons.CHECK_CIRCLE_O.getHtml();
+        }
+        return VaadinIcons.CIRCLE_THIN.getHtml();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener. ViewChangeEvent)
+     */
+    @Override
+    public void enter(ViewChangeEvent event) {
+        // Auto-generated method stub
+    }
+
+    @Override
+    public void receiveBroadcast(String message) {
+        getUI().access(() -> {
+            String gameId = player.getGameId();
+            readyButton.setEnabled(playerService.isMinPlayerCountOfGame(gameId));
+            reloadListe(myGrid, gameId);
+            if (!playerService.isMinPlayerCountOfGame(gameId)) {
+                playerReady.setValue("Zu wenig Spieler (Min. 3)");
+            } else {
+                playerReady.setValue("Warte auf Spieler");
+            }
+            if (playerService.playerOfGameReady(gameId)) {
+                startGame.setEnabled(true);
+                playerReady.setValue("Alle Spieler Bereit");
+            }
+            startGame(player.getId(), gameId);
+        });
+    }
 }
--- src/main/java/com/hacked/view/LoginView.java
+++ src/main/java/com/hacked/view/LoginView.java
@@ -1,9 +1,8 @@
 package com.hacked.view;
 
 import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
+import com.github.appreciated.material.MaterialTheme;
 import com.hacked.controller.Broadcaster;
 import com.hacked.controller.HackedSessionService;
 import com.hacked.controller.PlayerService;
@@ -15,73 +14,82 @@ import com.vaadin.server.UserError;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.ui.VerticalLayout;
 
 /**
- * @author  
+ * @author
  */
 @SpringView(name = LoginView.VIEW_NAME)
 @UIScope
-public class LoginView extends FormLayout implements View {
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
+public class LoginView extends VerticalLayout implements View {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	private TextField textFieldplayerName ;
-	
-	@Autowired
-	private PlayerService playerService;
+    private TextField textFieldplayerName;
 
-	public static final String VIEW_NAME = "login";
+    private TextField textFieldgameId;
 
-	@PostConstruct
-	void init() {
+    private Button buttonAnmelden;
 
-		String gameIdText = (String) UI.getCurrent().getSession().getAttribute(SessionKonstanten.GAME_ID);
-		TextField textFieldgameId = new TextField("Spiel-ID");
-		textFieldgameId.setValue(gameIdText);
-		textFieldplayerName = new TextField("Spielername");
-		Button buttonAnmelden = new Button("Anmelden");
-		buttonAnmelden.addStyleName(ValoTheme.BUTTON_SMALL);
-		buttonAnmelden
-				.addClickListener(e -> addPlayerToGame(textFieldgameId.getValue(), textFieldplayerName.getValue()));
-		addComponents(textFieldgameId, textFieldplayerName, buttonAnmelden);
+    @Autowired
+    private PlayerService playerService;
 
-	}
+    public static final String VIEW_NAME = "login";
 
-	/**
-	 * @param textFieldgameId
-	 * @param textFieldplayerName
-	 */
-	private void addPlayerToGame(String gameId, String playerName) {
-		if(playerService.isPlayerInGame(playerName, gameId)){
-			textFieldplayerName.setComponentError(new UserError("Spieler exestiert schon!"));
-			return;
-		}
-		
-		long id = playerService.addPlayerToGame(playerName, gameId);
-		Player player = playerService.getPlayer(id);
-		HackedSessionService.setGameId(gameId);
-		HackedSessionService.setPlayerId(player.getId());
-		HackedSessionService.setPlayer(player);
-		Broadcaster.broadcast(gameId, "Spieler " + playerName + " tritt dem Spiel bei.");
-		getUI().getNavigator().navigateTo(LobbyView.VIEW_NAME);
-	}
+    @PostConstruct
+    void init() {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.
-	 * ViewChangeEvent)
-	 */
-	@Override
-	public void enter(ViewChangeEvent event) {
-		// TODO Auto-generated method stub
+        String gameIdText = (String) UI.getCurrent().getSession().getAttribute(SessionKonstanten.GAME_ID);
+        textFieldgameId = new TextField("Spiel-ID");
+        textFieldgameId.setSizeFull();
+        textFieldgameId.setValue(gameIdText);
 
-	}
+        textFieldplayerName = new TextField("Spielername");
+        textFieldplayerName.setSizeFull();
+        buttonAnmelden = new Button("Anmelden");
+
+        buttonAnmelden.addStyleName(MaterialTheme.BUTTON_BORDERLESS);
+        buttonAnmelden.addClickListener(e -> addPlayerToGame(textFieldgameId.getValue(), textFieldplayerName.getValue()));
+        Panel container = new Panel();
+        VerticalLayout content = new VerticalLayout(textFieldgameId, textFieldplayerName, buttonAnmelden);
+        content.setSpacing(true);
+        content.setMargin(true);
+        container.setContent(content);
+        addComponents(container);
+
+    }
+
+    /**
+     * @param textFieldgameId
+     * @param textFieldplayerName
+     */
+    private void addPlayerToGame(String gameId, String playerName) {
+        if (playerService.isPlayerInGame(playerName, gameId)) {
+            textFieldplayerName.setComponentError(new UserError("Spieler exestiert schon!"));
+            return;
+        }
+
+        long id = playerService.addPlayerToGame(playerName, gameId);
+        Player player = playerService.getPlayer(id);
+        HackedSessionService.setGameId(gameId);
+        HackedSessionService.setPlayerId(player.getId());
+        HackedSessionService.setPlayer(player);
+        Broadcaster.broadcast(gameId, "Spieler " + playerName + " tritt dem Spiel bei.");
+        getUI().getNavigator().navigateTo(LobbyView.VIEW_NAME);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener. ViewChangeEvent)
+     */
+    @Override
+    public void enter(ViewChangeEvent event) {
+        // TODO Auto-generated method stub
+
+    }
 }

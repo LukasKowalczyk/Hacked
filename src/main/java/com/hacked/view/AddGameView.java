001a63ad55a11f817169b52d38aea825c9a7226b
LukasKowalczyk <kowalczyk.lukas.1987@gmail.com>
2017-12-23 19:54:25 +0100

    Anpassung Views

--- src/main/java/com/hacked/view/AddGameView.java
+++ src/main/java/com/hacked/view/AddGameView.java
@@ -1,11 +1,15 @@
 package com.hacked.view;
 
 import javax.annotation.PostConstruct;
+
+import com.github.appreciated.material.MaterialTheme;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -14,31 +18,48 @@ import com.vaadin.ui.VerticalLayout;
  */
 @SpringView(name = AddGameView.VIEW_NAME)
 public class AddGameView extends VerticalLayout implements View {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
-    public static final String VIEW_NAME = "addGame";
+	public static final String VIEW_NAME = "addGame";
 
-    @PostConstruct
-    void init() {
-        Label titel = new Label("Wilkommen!");
-        Label text = new Label("Klicke auf den Button um ein neues Spiel zu erstellen.");
-        Button addGame = new Button("Erstelle Spiel");
-        addGame.addClickListener(e -> {
-            UI.getCurrent().getNavigator().navigateTo(ShareGameView.VIEW_NAME);
-        });
-        addComponents(titel, text, addGame);
-        this.setSizeFull();
-    }
+	private Label titel;
+	private Label text;
+	private Button addGame;
 
-    /*
-     * (non-Javadoc)
-     * @see com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener. ViewChangeEvent)
-     */
-    @Override
-    public void enter(ViewChangeEvent event) {
-        // TODO Auto-generated method stub
-    }
+	@PostConstruct
+	void init() {
+		titel = new Label("Wilkommen!");
+		text = new Label("Klicke auf den Button um ein neues Spiel zu erstellen.");
+		addGame = new Button("Erstelle Spiel");
+		addGame.addStyleName(MaterialTheme.BUTTON_BORDERLESS);
+		addGame.addClickListener(e -> {
+			UI.getCurrent().getNavigator().navigateTo(ShareGameView.VIEW_NAME);
+		});
+
+		Panel container = new Panel();
+		VerticalLayout content = new VerticalLayout(titel, text, addGame);
+		content.setSpacing(true);
+		content.setMargin(true);
+		content.setComponentAlignment(titel, Alignment.MIDDLE_CENTER);
+		content.setComponentAlignment(text, Alignment.MIDDLE_CENTER);
+		content.setComponentAlignment(addGame, Alignment.MIDDLE_CENTER);
+		container.setContent(content);
+		addComponents(container);
+		this.setSizeFull();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.
+	 * ViewChangeEvent)
+	 */
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+	}
 }
--- src/main/java/com/hacked/view/ShareGameView.java
+++ src/main/java/com/hacked/view/ShareGameView.java
@@ -4,15 +4,19 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.github.appreciated.material.MaterialTheme;
 import com.hacked.controller.GameService;
 import com.hacked.controller.HackedSessionService;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.StreamResource;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -21,48 +25,68 @@ import com.vaadin.ui.VerticalLayout;
  */
 @SpringView(name = ShareGameView.VIEW_NAME)
 public class ShareGameView extends VerticalLayout implements View {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
-    public static final String VIEW_NAME = "shareGame";
+	public static final String VIEW_NAME = "shareGame";
 
-    @Autowired
-    private GameService gameService;
+	@Autowired
+	private GameService gameService;
 
-    @PostConstruct
-    void init() {
-        String id = gameService.generateGame();
-        Label gameId = new Label("GameId : " + id);
-        Image qrCode = new Image();
-        qrCode.setSource(new StreamResource(new StreamResource.StreamSource() {
-            /**
-            *
-            */
-            private static final long serialVersionUID = 1L;
+	private Label gameId;
 
-            @Override
-            public InputStream getStream() {
-                return new ByteArrayInputStream(gameService.getQR(id));
+	private Image qrCode;
 
-            }
-        }, ""));
-        Button addGame = new Button("Login");
-        addGame.addClickListener(e -> {
-            HackedSessionService.setGameId(id);
-            UI.getCurrent().getNavigator().navigateTo(LoginView.VIEW_NAME);
-        });
-        addComponents(gameId, qrCode, addGame);
-        this.setSizeFull();
-    }
+	private Button joinGame;
 
-    /*
-     * (non-Javadoc)
-     * @see com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener. ViewChangeEvent)
-     */
-    @Override
-    public void enter(ViewChangeEvent event) {
-        // TODO Auto-generated method stub
-    }
+	@PostConstruct
+	void init() {
+		String id = gameService.generateGame();
+		gameId = new Label("GameId : " + id);
+		qrCode = new Image();
+		qrCode.setSource(new StreamResource(new StreamResource.StreamSource() {
+			/**
+			*
+			*/
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public InputStream getStream() {
+				return new ByteArrayInputStream(gameService.getQR(id));
+
+			}
+		}, ""));
+		joinGame = new Button("Login");
+		joinGame.addStyleName(MaterialTheme.BUTTON_BORDERLESS);
+		joinGame.addClickListener(e -> {
+			HackedSessionService.setGameId(id);
+			UI.getCurrent().getNavigator().navigateTo(LoginView.VIEW_NAME);
+		});
+		
+		Panel container = new Panel();
+		VerticalLayout content = new VerticalLayout(gameId, qrCode, joinGame);
+		content.setSpacing(true);
+		content.setMargin(true);
+		content.setComponentAlignment(gameId, Alignment.MIDDLE_CENTER);
+		content.setComponentAlignment(qrCode, Alignment.MIDDLE_CENTER);
+		content.setComponentAlignment(joinGame, Alignment.MIDDLE_CENTER);
+		container.setContent(content);
+		addComponents(container);
+		
+		this.setSizeFull();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.
+	 * ViewChangeEvent)
+	 */
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+	}
 }
